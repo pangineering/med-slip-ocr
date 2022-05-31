20f46a7437d7da473b56aa53b7f6d6a312e4735c
pantkdkrt <p.engineering6415@gmail.com>
2022-05-31 19:58:10 +0700

    fix 'detect_module'
    can detect and crop out

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-from Modules.dataset_module import Dataset
 from Modules.detect_module import Detect
+
+
+model_path="models/model.pt"
+
+detect = Detect(model_path=model_path,yolo='custom')
+
+img = 'Images/p97pnrb8m11rRObnjEt-o.jpg' 
+
+detect.run(img)
