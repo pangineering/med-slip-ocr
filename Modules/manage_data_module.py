6f8fe336c5d05692e6c2bc34174989ec73509744
pantkdkrt <p.engineering6415@gmail.com>
2022-06-01 15:05:05 +0700

    add manage_image_module

--- Modules/manage_data_module.py
+++ Modules/manage_data_module.py
@@ -2,29 +2,33 @@ import cv2
 import PIL
 from PIL import Image as im
 from PIL import ImageDraw
+import os
 
 # Load data
 class Dataset:
-    def __init__(self,img_file="",qty="single"):
+    def __init__(self,img_file=""):
         self.img_path = img_file
-        self.qty = qty #'single' for one image and 'multi' for more than one
+
 
     def readImage(self):
-        if self.qty=="single":
-            pass
-        else:
-            pass
+        # Get images into array
+        img_file = []
+        for filename in os.listdir(self.img_path):
+            f = os.path.join(self.img_path, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                img_file.append(f)
+
+        return img_file
+
 
     def showImage(self):
-        if self.qty=="single":
-            pass
-        else:
-            pass
+        pass
 
     def saveImage(self):
-        if self.qty=="single":
-            pass
-        else:
-            pass
+        pass
 
+
+class Output:
+    pass
         
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import cv2
+from Modules.manage_data_module import Dataset
 from Modules.detect_module import Detect
 from Modules.ocr_module import Read_text
 from PIL import Image as im
@@ -8,16 +9,18 @@ model_path="models/model.pt"
 detect = Detect(model_path=model_path,yolo='custom')
 
 
-img = 'Images/p97pnrb8m11rRObnjEt-o.jpg' 
-img_1 = im.open(img)
+#img = 'Images/p97pnrb8m11rRObnjEt-o.jpg' 
+#img_1 = im.open(img)
+img_data = Dataset(img_file="Images/")
 
+img_list = img_data.readImage()
 
-detect.run(img_1)
+detect.run(img_list)
 
-result = ["runs/detect/exp6/crops/sys/p97pnrb8m11rRObnjEt-o.jpg"]
+#result = ["runs/detect/exp6/crops/sys/p97pnrb8m11rRObnjEt-o.jpg"]
 
-read = Read_text(img=result)
+#read = Read_text(img=result)
 
-res = read.easy_read()
+#res = read.easy_read()
 
-print(res)
+print(detect)
